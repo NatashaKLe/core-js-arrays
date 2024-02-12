f72c059078fd1ed2e8369fba32fdd167c24fc3c8
Natashakle <ataska91@gmail.com>
2024-02-12 21:54:40 +0300

    feat: add 39 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -393,7 +393,7 @@ function getElementByIndices(arr, indices) {
     arr
   );
 }
-// console.log(...indices.map((x) => [x]));
+
 /**
  * Returns the number of all falsy values in the specified array.
  *
@@ -406,8 +406,9 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const result = arr.filter((el) => !el);
+  return result.length;
 }
 
 /**
