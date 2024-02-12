5e154a60408475e2aa9979f8b35f56a95043cb29
Natashakle <ataska91@gmail.com>
2024-02-13 00:24:33 +0300

    feat: add 46 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -555,8 +555,18 @@ function findLongestIncreasingSubsequence(nums) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  const result = [];
+  if (!arr) return [];
+  arr.map((el, i) => {
+    const arr1 = Array(i + 1).fill('');
+    arr1.map(() => {
+      result.push(el);
+      return el;
+    });
+    return el;
+  });
+  return result;
 }
 
 /**
