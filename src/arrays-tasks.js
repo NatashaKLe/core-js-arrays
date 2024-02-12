84aac670368bf117ab21e85a201a236d8ef3f5fd
Natashakle <ataska91@gmail.com>
2024-02-12 23:58:12 +0300

    feat: add 45 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -526,8 +526,19 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  const maxLength = [];
+  let count = 1;
+  nums.map((el, i) => {
+    if (el < nums[i + 1]) {
+      count += 1;
+      maxLength.push(count);
+    } else {
+      count = 1;
+    }
+    return el;
+  });
+  return maxLength.sort((a, b) => b - a)[0];
 }
 
 /**
